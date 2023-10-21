b206c514102f81d00b87cd2a217724484c46f456
Thao Stein <thaostein1@gmail.com>
2023-10-21 16:15:21 +0200

    init

---
+++ src/main/java/htwberlin/webtech/KathaoApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KathaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KathaoApplication.class, args);
+	}
+
+}
