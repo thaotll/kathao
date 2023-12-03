91b9afea3793d2016fd9439d6d9bd3cf7a1fa16e
Thao Stein <thaostein1@gmail.com>
2023-12-03 15:42:08 +0100

    created postgresql database

--- src/main/java/htwberlin/webtech/Stellenangebot.java
+++ src/main/java/htwberlin/webtech/Stellenangebot.java
@@ -1,11 +1,9 @@
 package htwberlin.webtech;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table
 public class Stellenangebot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
